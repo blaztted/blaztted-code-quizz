2039a8ccd55bef5d1cc21b7a39a363941b6c1145
blaztted <bbc.silva@gmail.com>
2023-12-22 18:42:35 +0000

    created all folders and html, js and css files

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// Create a code quiz that contains the following requirements:
+
+// A start button that when clicked a timer starts and the first question appears.
+
+// Questions contain buttons for each answer.
+// When answer is clicked, the next question appears
+// If the answer clicked was incorrect then subtract time from the clock
+// The quiz should end when all questions are answered or the timer reaches 0.
+
+// When the game ends, it should display their score and give the user the ability to save their initials and their score
+//-------------------------------//
+// Set of questions --> array of objects
+// Each question needs the following:
+// Question text
+// Set of answers
+// Which answer is correct
+
+// Landing page:
+// Explanation of the quiz
+// Start button
+
+// Click the start button:
+// Landing page goes away
+// Timer starts
+// The first question appears (with its answers)
+
+// For each question:
+// User clicks an answer
+// Their choice is compared to the correct answer as stored in the question's object
+// If correct, tell them
+// If incorrect, tell them AND subtract time from the timer
+// Optional: play a sound for correct or incorrect
+// Either way, the question disappears after a few seconds and the next question appears
+
+// After the last question:
+// Timer stops
+// Question disappears
+// Form appears for user to enter their initials
+// Display their score
+
+// User submits form
+// Initials and score get stored in local storage
+// User is taken to the high scores page
+// High scores are listed, sorted highest to lowest
+// User has option to take the quiz again
